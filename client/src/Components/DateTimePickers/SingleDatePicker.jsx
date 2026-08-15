cea840d6d34f4d13cd3facc7c22513804df98fd2
GeoPapandreou <georgepap091@gmail.com>
2026-08-15 19:40:39 +0300

    Date and Time Picker changes

--- client/src/Components/DateTimePickers/SingleDatePicker.jsx
+++ client/src/Components/DateTimePickers/SingleDatePicker.jsx
@@ -1,36 +1,15 @@
-import { useState } from "react";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 
-const SingleDatePicker = ({
-  OnDateChanged,
-  Label = "Date",
-  DisablePast = false,
-  HasFullWidth = true
-}) => {
-  const [SelectedDate, SetSelectedDate] = useState(null);
-
-  const HandleDateChanged = (NewDate) => {
-    SetSelectedDate(NewDate);
-    OnDateChanged?.(NewDate);
-  };
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DatePicker
-        label={Label}
-        value={SelectedDate}
-        onChange={HandleDateChanged}
-        disablePast={DisablePast}
-        slotProps={{
-          textField: {
-            fullWidth: HasFullWidth
-          }
-        }}
-      />
-    </LocalizationProvider>
-  );
-};
+const SingleDatePicker = ({OnDateChanged}) => (
+  <LocalizationProvider dateAdapter={AdapterDateFns}>
+    <DatePicker
+      label="Date"
+      onChange={OnDateChanged}
+      disablePast
+    />
+  </LocalizationProvider>
+);
 
 export default SingleDatePicker;
